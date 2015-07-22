202b2c0f12adbe05ec252b9fd916373a89326494
RonaldAtGitHub <github@dhamecourt.com>
2015-07-22 09:15:22 +0200

    First commit of fresh project

---
+++ nl.c2c.ac.test/src/nl/c2c/ac/test/github/GitHub.java
@@ -0,0 +1,21 @@
+package nl.c2c.ac.test.github;
+
+
+public class GitHub  {
+
+	private static String context;
+
+	public static String getContext() {
+		return context;
+	}
+
+	public static void setContext(String context) {
+		GitHub.context = context;
+	}
+
+	public GitHub(){
+		
+	}
+	
+
+}
